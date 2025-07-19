2be1494f2001f09f28f3f98d84358ee67e3ddc90
bobsterrr <lobanovdaniilll@gmail.com>
2025-07-19 18:35:51 +0200

    fetch: ToDo functionality added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
 <script setup>
-import {ref} from 'vue'
+import { ref, computed } from 'vue';
 
-const count = ref(0);
+const todos = ref([]);
+const newTodo = ref('');
+const hideCompleted = ref(false);
 
+let id = todos.value.length > 0 ? Math.max(...todos.value.map(todo => todo.id)) + 1 : 1;
+
+const filteredTodos = computed(() => {
+  return hideCompleted.value
+    ? todos.value.filter((todo) => !todo.done)
+    : todos.value;
+});
+
+// Methods
+function addTodo() {
+  if (newTodo.value.trim() === '') {
+    return;
+  }
+  todos.value.push({
+    id: id++,
+    text: newTodo.value,
+    done: false
+  });
+  newTodo.value = '';
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo);
+}
 </script>
 
 <template>
-  <h1>To Do List</h1>
-  <input placeholder="Make New ToDo">
-  <button>Add Todo</button>
-  <button @click='count++'>Count is {{count}}</button>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo">
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button id='todo_deleting' @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
+  </button>
 </template>
 
-<style scoped>
-h1 {
-  text-align: center;
-  font-weight: bold;
-  color: white
+<style>
+.done {
+  text-decoration: line-through;
+}
+
+ul {
+  all: unset;
+}
+
+li {
+  all: unset;
 }
 
 </style>
