6ef3e3178c01f897c3e2162ff44028f1e9322ac6
bobsterrr <lobanovdaniilll@gmail.com>
2025-07-19 20:19:02 +0200

    fetch: add todo input

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,15 @@
   <div id='container'>
     <div id='todo-app'>
       <h2>To-Do List</h2>
+      <div id='row'>
+        <input type='text' id='input-box' placeholder='Add Your Text'>
+        <button>Add</button>
+      </div>
+      <ul id='list-container'>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -15,6 +24,16 @@
   padding: 0;
   font-family: 'Poppins', sans-serif;
   box-sizing: border-box;
+}
+
+button{
+  border: none;
+   outline: none;
+  padding: 16px 50px;
+  background: #ff5945;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 40px;
 }
 
 #container {
@@ -39,5 +58,31 @@
   display: flex;
   align-items: center;
   margin-bottom: 20px;
+}
+
+#row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: #edeef0;
+  border-radius: 30px;
+  padding-left: 20px;
+  margin-bottom: 25px;
+}
+
+input {
+  flex: 1;
+  border: none;
+  background: transparent;
+  padding: 10px;
+  font-weight: 14px;
+}
+
+ul li {
+  list-style: none;
+  font-size: 17px;
+  padding: 12px 8px 12px 50px;
+  user-select: none;
+  cursor: pointer;
 }
 </style>
